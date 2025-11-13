b7d9f635fc098b85b0edbb8b263225e5ef60338a
Дарья Попова <A1111.1112@yandex.ru>
2025-11-13 18:12:59 +0300

    first

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,24 +1,18 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+export function initFiltering(elements) {
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
+                const el = document.createElement('option');
+                el.textContent = name;
+                el.value = name;
+                return el;
+            }))
+        })
+    }
 
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
-
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes).forEach((elementName) => {
-        elements[elementName].append(
-            ...Object.values(indexes[elementName]).map(name => {
-                const option = document.createElement("option");
-                option.value = name;
-                option.textContent = name;
-                return option;
-            })
-        );
-    });
-    return (data, state, action) => {
+    const applyFiltering = (query, state, action) => {
         // @todo: #4.2 — обработать очистку поля
         if (action?.name === "clear") {
-            // Находим input, связанный с кнопкой очистки
             const parent = action.closest("[data-field]");
             if (parent) {
                 const input = parent.querySelector("input, select");
@@ -26,11 +20,27 @@ export function initFiltering(elements, indexes) {
 
                 if (input) input.value = "";
                 if (fieldName && state[fieldName] !== undefined) {
-                    state[fieldName] = ""; // сбрасываем значение и в state
+                    state[fieldName] = "";
                 }
             }
         }
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state));
+
+        // @todo: #4.5 — сформировать параметры фильтрации для сервера
+        const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) {
+                    // Формируем параметры фильтра для сервера
+                    filter[`filter[${elements[key].name}]`] = elements[key].value;
+                }
+            }
+        });
+
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
+    }
+
+    return {
+        updateIndexes,
+        applyFiltering
     }
 }
--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import {initSearching} from "./components/searching.js";
 // @todo: подключение
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const api = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
@@ -37,16 +37,18 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
+async function render(action) {
     let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
-    result = applySearch(result, state, action);
-    result = applyFiltering(result, state, action);
-    result = applySorting(result, state, action);
-    result = applyPagination(result, state, action);
+    let query = {}; // здесь будут формироваться параметры запроса
+    query = applySearching(query, state, action);
+    query = applyFiltering(query, state, action);
+    query = applySorting(query, state, action);
+    query = applyPagination(query, state, action); // обновляем query
 
-    sampleTable.render(result)
+    const { total, items } = await api.getRecords(query); // запрашиваем данные с собранными параметрами
+
+    updatePagination(total, query); // перерисовываем пагинатор
+    sampleTable.render(items);
 }
 
 const sampleTable = initTable({
@@ -57,18 +59,16 @@ const sampleTable = initTable({
 }, render);
 
 // @todo: инициализация
-const applySearch = initSearching('search');
+const applySearching = initSearching('search');
 
-const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
-    searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
-});
+const {applyFiltering, updateIndexes} = initFiltering(sampleTable.filter.elements);
 
 const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
     sampleTable.header.elements.sortByDate,
     sampleTable.header.elements.sortByTotal
 ]);
 
-const applyPagination = initPagination(
+const {applyPagination, updatePagination} = initPagination(
     sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
     (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
         const input = el.querySelector('input');
@@ -81,6 +81,15 @@ const applyPagination = initPagination(
 );
 
 const appRoot = document.querySelector('#app');
+
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+    const indexes = await api.getIndexes();
+
+    updateIndexes(sampleTable.filter.elements, {
+        searchBySeller: indexes.sellers
+    });
+}
+
+init().then(render);
